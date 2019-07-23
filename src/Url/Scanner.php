28db4d3e5027f56854d27a44698512c72a952e08
Joe.Chan <cshujun21625@gmail.com>
2019-07-23 16:54:45 +0800

    composer private repository

---
+++ src/Url/Scanner.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Just for learning not commerical
+ *
+ * @author    Joe.Chan<cshujun21625@gmail.com>
+ * @date      2019年7月23日 下午4:03:18
+ * @link      git@github.com:yuanmingtao/l1.git
+ */
+namespace Oreilly\ModernPHP\Url;
+class Scanner
+{
+    /**
+     * @var array An array of URLs
+     */
+    protected $urls;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $httpClient;
+    /**
+     * Constructor
+     * @param array $urls An array of URLs to scan
+     */
+    public function __construct(array $urls)
+    {
+        $this->urls = $urls;
+    }
+    /**
+     * Get invalid URLs
+     * @return array
+     */
+    public function getInvalidUrls()
+    {
+        $invalidUrls = [];
+        foreach($this->urls as $url) {
+            try {
+                $statusCode = $this->getStatusCodeForUrl($url);
+            } catch (\Exception $e) {
+                $statusCode = 500;
+            }
+            
+            if ($statusCode >= 400) {
+                array_push($invalidUrls, [
+                    'url' => $url,
+                    'status' => $statusCode
+                ]);
+            }
+        }
+        
+        return $invalidUrls;
+    }
+    
+    /**
+     * Get HTTP status code for URL
+     * @param string $url The remote URL
+     * @return int The HTTP status code
+     */
+    protected function getStatusCodeForUrl($url)
+    {
+        $httpResponse = $this->httpClient->options($url);
+        return $httpResponse->getStatusCode();
+    }
+}
